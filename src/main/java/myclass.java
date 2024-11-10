e45f73da53fd821ad5787d82ebc9b0985f113a4e
unknown <ankityaddav002@gmail.com>
2024-11-10 14:31:38 +0530

    added service class

---
+++ src/main/java/myclass.java
@@ -0,0 +1,15 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class myclass {
+	
+	int s=10;
+	int b=20;
+	
+	int c=s+b;
+	
+	public void sum() {
+		System.out.println(c);
+	}
+
+}
--- src/main/java/todo/controller/TodoController.java
+++ src/main/java/todo/controller/TodoController.java
@@ -1,17 +1,31 @@
 package todo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import todo.service.TodoService;
+
 
 @RestController
 @RequestMapping("/todo")
 public class TodoController {
 	
+	@Autowired
+	private TodoService service;
 	
 	@GetMapping("/hitodo")
 	public String getTodoIntro() {
 		return "Hi I am todo list";
 	}
+	
+	
+	@GetMapping("/mysum")
+	public int sum() {
+		return service.sum();
+	}
+	
+	
 
 }
